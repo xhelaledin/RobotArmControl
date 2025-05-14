379395762975a016639d878835abd0919c964c2c
xhelaledin <xhelaledin@gmail.com>
2025-05-14 04:57:55 +0200

    Improvements on ui
    Changed the 3d model positions to match the physical Arm

--- Assets/RobotArmInputHandler.cs
+++ Assets/RobotArmInputHandler.cs
@@ -21,14 +21,14 @@ public class RobotArmInputHandler : MonoBehaviour
     public void SetBaseRotation(float zRotation)
     {
         Debug.Log($"Setting Base Rotation: {zRotation}");
-        baseCylinder.localEulerAngles = new Vector3(180f, 0f, zRotation*1.8f);
+        baseCylinder.localEulerAngles = new Vector3(180f, 0f, -zRotation*1.8f);
     }
 
     // Method to set Joint1 rotation (controlled by slider)
     public void SetJoint1Rotation(float yRotation)
     {
         Debug.Log($"Setting Joint1 Rotation: {yRotation}");
-        joint1.localRotation = Quaternion.Euler(270f, yRotation-50, 0.185f);
+        joint1.localRotation = Quaternion.Euler(270f, -yRotation+80, 0.185f);
     }
 
     // Method to set Joint2 rotation (controlled by slider)
@@ -38,7 +38,7 @@ public class RobotArmInputHandler : MonoBehaviour
 
         if (activeScene.name == "RobotArmScene")
     {
-        if (yRotation > 41 && yRotation < 158)
+        if (yRotation > 41 )
         {
             robotArmModel.position = new Vector3(2.74f, 3.1f, -40.6f);
         }
@@ -59,7 +59,7 @@ public class RobotArmInputHandler : MonoBehaviour
         }
     }
         Debug.Log($"Setting Joint2 Rotation: {yRotation}");
-        joint2.localRotation = Quaternion.Euler(270f, yRotation-160, 129.6f);
+        joint2.localRotation = Quaternion.Euler(270f, -yRotation+40, 129.6f);
     }
 
     // Method for "Set Button" to set specific positions for Joint3R and Joint3L
